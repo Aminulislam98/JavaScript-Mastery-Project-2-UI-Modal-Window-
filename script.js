dc4b2547ee4d66ca211c9274d1503fd1eda2bc8d
Aminul islam <aminurrohman98@gmail.com>
2026-06-22 19:09:56 +0100

    Learned how to open modal and closed back by manipulating DOM

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ const modal = document.querySelector('.modal');
 const btnClosed = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+btnClosed.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
