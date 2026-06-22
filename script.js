04d9c137f8cfff88b6008497bfe73dcff4dcac60
Aminul islam <aminurrohman98@gmail.com>
2026-06-22 17:56:54 +0100

    access all element through dom

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const btnClosed = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
